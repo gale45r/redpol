e6e66648182952680ebf0ffece7c94f1e6aaca63
ariel-2375 <mirarluar@gmail.com>
2019-09-05 15:56:29 -0300

    9 hh

--- index.php
+++ index.php
@@ -5,5 +5,4 @@ echo "El index.php que se debe ejecutar está dentro de la carpeta controller";
 echo "<br>";
 //primera llamada al controlador --> index.php dentro de la carpeta controller
 echo " controller/index.php ";
-//hola como andas 
-?>
+//hola como andas gcgncgcgb>
