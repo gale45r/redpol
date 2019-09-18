8d3dcc201160306826f5cd81c019f49d1648dac3
ariel-2375 <mirarluar@gmail.com>
2019-09-18 17:52:28 -0300

    ok

--- views/bienvenido_view.php
+++ views/bienvenido_view.php
@@ -1,24 +1,20 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-		<title><?php echo $title; ?></title>      
+        <title><?php echo $title; ?></title>  
+        <link rel="stylesheet" href="../css2/estilos.css">    
 
     </head>
     <body>
 		<header>
-			<div id="Menu">
-				<h1 id="title"><?php echo $title; ?></h1>
-				<a href="../controller/controlerLogin.php">Iniciar Sesi&oacute;n</a>
-				<a href="../controller/registroUsuario_controller.php">Registrarse</a>
-			</div>
-		</header>
-
-        <div id="menu">
-            <h2>Men&uacute;</h2>
-            <ul>
+			<div class="menu">
+				<h1 id="title"><?php echo $title; ?></h1>				
                 <li><a href="index.php">Inicio</a></li>
                 <li><a href="../controller/listarProductos_controller.php">Listar Productos</a></li>
                 <li><a href="#">Ver Carrito</a></li>
+                <li> <a href="../controller/controlerLogin.php">Iniciar Sesi&oacute;n</a></li>
+                <li><a href="../controller/registroUsuario_controller.php">Registrarse</a></li>
+			
             </ul>
         </div>
         <div id="texto">
